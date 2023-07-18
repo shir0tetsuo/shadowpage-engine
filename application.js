3517b643f66874dab6a5d48031091e25c0af82fc
shir0tetsuo <shadowsword@protonmail.com>
2023-07-18 11:38:53 -0400

    General cleanup and additions

--- application.js
+++ application.js
@@ -2,7 +2,46 @@ const express = require('express');
 const app = express();
 const crypto = require('crypto-js');
 const { Sequelize, DataTypes } = require('sequelize');
-require('dotenv').config();
+// According to huggingchat
+var googleapis = require('googleapis')
+var auth = require('google-auth-library')
+const { ClosureSystem } = require('closure-library')
+
+// TODO: We don't have the priv key because it's on the other server.
+//const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY) // $ => G,S
+
+const bcrypt = require('bcrypt') // https://www.npmjs.com/package/bcrypt
+const saltRounds = 10;
+
+const bparse = require('body-parser') //https://codeforgeek.com/handle-get-post-request-express-4/
+const cookies = require('cookie-parser') //https://stackoverflow.com/questions/16209145/how-to-set-cookie-in-node-js-using-express-framework
+
+
+// Fn
+
+async function readFile(filePath) {
+  try {
+    const data = await fs.readFile(filePath);
+    //console.log(data.toString());
+    return data.toString()
+  } catch (error) {
+    console.error(`Got an error trying to read the file: ${error.message}`);
+  }
+}
+
+function zeroPad(num, places) {
+  var zero = places - num.toString().length + 1;
+  return Array(+(zero > 0 && zero)).join("0") + num;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function genID() {
+  return `1P${zeroPad(getRandomInt(999),3)}X${Date.now()}`
+}
+
 
 // Load environment variables from .env file
 require('dotenv').config();
@@ -15,11 +54,6 @@ const sequelize = new Sequelize({
 
 // Define the User model
 const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
-  },
   name: {
     type: DataTypes.STRING,
     allowNull: false
@@ -27,8 +61,24 @@ const User = sequelize.define('User', {
   email: {
     type: DataTypes.STRING,
     allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
   }
 });
+
+User.sync()
+
+
+app.use(express.json());
+app.use(bparse.urlencoded({ extended: true }));
+app.use(bparse.json());
+app.use(cookies())
+// load /img/ media from folder 'img'
+app.use('/static', express.static('static'));
+//app.use('/favicon.ico', express.static('favicon.ico'));
+app.use('/robots.txt', express.static('robots.txt'));
 
 // Express configuration
 app.use(express.static('static'));
@@ -42,11 +92,14 @@ function loadStaticFile(filename) {
 app.get('/', (req, res) => {
   // Google authentication implementation goes here
   // If user is logged in with Gmail, display contents from passed-user-login.html
+  console.log(res)
 });
 
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  // Preload ClosureSystem on app listen
+  //ClosureSystem.preload
+  console.log(`The server has started on port ${port}`);
 });
 
--- npm.sh
+++ npm.sh
@@ -3,3 +3,7 @@
 /usr/local/bin/npm install --save sequelize
 /usr/local/bin/npm install --save sqlite3
 /usr/local/bin/npm install --save express
+/usr/local/bin/npm install --save dotenv
+/usr/local/bin/npm install --save googleapis google-auth-library closure-library
+/usr/local/bin/npm install --save stripe bcrypt
+/usr/local/bin/npm install --save body-parser cookie-parser
